3809529f1063e2e0422bceab0059b373033c0824
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-01-19 15:25:44 +0300

    implemented routing

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+
 const CityCard = () => {
+  let navigate = useNavigate();
+  const handleNavigation = (city) => {
+    navigate(`${city}`)
+  }
   return (
     <>
     <h1>City Name</h1>
     <h2>Country</h2>
-    <button className='view-polution-btn'>
+    <button className='view-polution-btn' onClick={() => handleNavigation('city')}>
     view polution
     </button>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import HomePage from './pages/home-page'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Pollution from './components/Pollution'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: ':city',
+    element: <Pollution />
+  },
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
